475966f1ecccb879498c7cbdf0202f7814c46a46
Parkwochang <whwh3560@gmail.com>
2022-06-14 21:13:07 +0900

    React and gql connection

--- movieql-client/src/routes/Movies.jsx
+++ movieql-client/src/routes/Movies.jsx
@@ -1,3 +1,42 @@
+import { gql, useQuery } from "@apollo/client";
+
+const GET_MOvIES = gql`
+  query allMovies {
+    allMovies {
+      title
+      id
+    }
+    allTweets {
+      id
+      text
+      author {
+        fullName
+      }
+    }
+  }
+`;
+
 export default function Movies() {
-  return <div>This is a list of movies.</div>;
+  const { data, loading, error } = useQuery(GET_MOvIES);
+  console.log(loading);
+  if (loading) {
+    return <h1>Loading...</h1>;
+  }
+  if (error) return <h1>Could not fetch </h1>;
+  return (
+    <>
+      <ul>
+        <h1>Movies</h1>
+        {data.allMovies.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+        <h1>Tweets</h1>
+        {data.allTweets.map((tweet) => (
+          <li key={tweet.id}>
+            {tweet.text} by: {tweet.author.fullName}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
 }
